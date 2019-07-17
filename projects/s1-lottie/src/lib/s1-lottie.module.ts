2de9294fd87f6a9f7f4642f29d2c3085d6cb8f56
oz gonen <ozg@sentinelone.com>
2019-07-17 14:19:23 +0300

    new demo

---
+++ projects/s1-lottie/src/lib/s1-lottie.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { S1LottieComponent } from './s1-lottie.component';
+import {CommonModule} from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [S1LottieComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [S1LottieComponent]
+})
+export class S1LottieModule { }
