28767ed2c085094d906aec96a9cd38e1cf8d3121
lironh <lironenk@gmail.com>
2019-10-28 15:07:58 +0200

    feat(s1-lottie): added heart anim!

--- src/app/demo/anim-cube/anim-cube.component.ts
+++ src/app/demo/anim-cube/anim-cube.component.ts
@@ -1,28 +1,21 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-anim-cube',
   templateUrl: './anim-cube.component.html',
   styleUrls: ['./anim-cube.component.css']
 })
-export class AnimCubeComponent implements OnInit, OnDestroy {
-  scaled = false;
-  rAF;
+export class AnimCubeComponent implements OnInit {
+
+  @Input() set loopCount(count) {
+    this.hearts.push(count);
+  }
+
+  hearts = [];
+
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onClick() {
-    this.rAF = requestAnimationFrame(() => {
-      this.scaled = !this.scaled;
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.rAF) {
-      cancelAnimationFrame(this.rAF);
-    }
   }
 
 }
